ef0d33d9486fae52b964656f89d026c1e710074f
Faadil Torou <ftorou@gmail.com>
2023-02-23 09:50:14 +0100

    adding vue elements

--- PSaclaide/resources/js/app.js
+++ PSaclaide/resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import NavBar from './components/Navbr.vue'
+import Footer from './components/Footer.vue'
+
+const app = createApp({});
+
+app.component('nav-bar', NavBar);
+
+app.component('footer-bar',Footer);
+
+app.mount('#app');
+
